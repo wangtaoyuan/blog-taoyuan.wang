c1a8b073e563f78a274bfeddd968af6b96b70932
wangtaoyuan <hdwangtaoyuan@126.com>
2018-05-09 20:27:27 +0800

    初步显示首页，超链接到文章

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from datetime import datetime
+from django.template.loader import get_template
+from django.shortcuts import redirect
 
 # Create your views here.
 
 
 def homepage(request):
+    # posts = Post.objects.all()
+    # post_lists = list()
+    # for count, post in enumerate(posts):
+    #     post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
+    #     post_lists.append("<small>" + str(post.body) + "</small><br><br>")
+
+    template = get_template('index.html')
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts):
-        post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
+
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/') #直接返回首页
